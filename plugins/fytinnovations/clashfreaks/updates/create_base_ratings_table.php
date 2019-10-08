23d99f9c8a3ac1602ff2cb9beb6c6ec9f4bd67d3
Aniket Magadum <aniketmagadum@fytinnovations.com>
2019-10-08 19:48:40 +0530

    Deleted demo themes

--- plugins/fytinnovations/clashfreaks/updates/create_base_ratings_table.php
+++ plugins/fytinnovations/clashfreaks/updates/create_base_ratings_table.php
@@ -17,15 +17,18 @@ class CreateBaseRatingsTable extends Migration
             $table->integer('base_design_id')->unsigned();
             $table->integer('ratings')->unsigned();
             $table->timestamps();
-            $table->foreign('user_id', 'base_ratings_user_id')->references('id')->on('users');
-            $table->foreign('base_design_id', 'base_ratings_base_design_id')->references('id')->on('fytinnovations_clashfreaks_base_designs');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('base_design_id')->references('id')->on('fytinnovations_clashfreaks_base_designs');
         });
     }
 
     public function down()
     {
         Schema::table('fytinnovations_clashfreaks_base_ratings', function ($table) {
-            $table->dropForeign('base_ratings_user_id', 'base_ratings_base_design_id');
+            $table->dropForeign(
+                'fytinnovations_clashfreaks_base_ratings_user_id_foreign',
+                'fytinnovations_clashfreaks_base_ratings_base_design_id_foreign'
+            );
         });
         Schema::dropIfExists('fytinnovations_clashfreaks_base_ratings');
     }
