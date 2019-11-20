1ed3c622f2a6a90dc00aad0199e4784c48b142e1
Aniket Magadum <aniketmagadum@fytinnovations.com>
2019-11-20 10:53:06 +0530

    Updated service worker and offline support

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'clashfreaks-cache';
+
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+    '/',
+    '/offline',
+];
+
+self.addEventListener('install', (evt) => {
+    // CODELAB: Precache static resources here.
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+    // CODELAB: Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('offline');
+                    });
+            })
+    );
+});
